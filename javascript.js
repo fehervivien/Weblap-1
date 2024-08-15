3931538f4c90b8fa9d81d48e0d74dfa03c9cb351
fvf <fehervivien96@gmail.com>
2024-08-15 20:45:56 +0200

    Itt már a header-img divben benne van a kép.

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    //header.style.backgroundColor = 'blue';
+    console.log('JavaScript fájl sikeresen betöltve!');
+});
